3efcb92db4ac362b0016feb5ea1388e3e413c2dd
unknown <fachrezaghifary@gmail.com>
2020-12-08 09:15:19 +0700

    last updated

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check-auth');
@@ -30,7 +30,6 @@ module.exports = {
     Mutation: {
         async createPost(_, { body }, context) {
             const user = checkAuth(context);
-            console.log(user);
 
             const newPost = new Post({
                 body,
@@ -40,6 +39,10 @@ module.exports = {
             });
 
             const post = await newPost.save();
+
+            context.pubsub.publish('NEW_POST', {
+                newPost: post
+            });
 
             return post;
         },
@@ -57,6 +60,32 @@ module.exports = {
             } catch (err) {
                 throw new Error(err);
             }
+        },
+        likePost: async (_, { postId }, context) => {
+            const { username } = checkAuth(context);
+
+            const post = await Post.findById(postId);
+            if (post) {
+                if (post.likes.find((like) => like.username === username)) {
+                    //Post already liked => unlike it
+                    post.likes = post.likes.filter(
+                        (like) => like.username !== username
+                    );
+                } else {
+                    //Post not Liked => like it
+                    post.likes.push({
+                        username,
+                        createdAt: new Date().toISOString()
+                    });
+                }
+                await post.save();
+                return post;
+            } else throw new UserInputError('Post not found');
+        }
+    },
+    Subscription: {
+        newPost: {
+            subscribe: (_, __, { pubsub }) => pubsub.asyncIterator('NEW_POST')
         }
     }
 };
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -6,6 +6,23 @@ module.exports = gql`
         body: String!
         createdAt: String!
         username: String!
+        comments: [Comment]!
+        likes: [Like]!
+        likeCount: Int!
+        commentCount: Int!
+    }
+
+    type Comment {
+        id: ID!
+        body: String!
+        username: String!
+        createdAt: String!
+    }
+
+    type Like {
+        id: ID!
+        username: String!
+        createdAt: String!
     }
 
     type User {
@@ -25,7 +42,7 @@ module.exports = gql`
 
     type Query {
         getPosts: [Post]
-ad        getPost(postId: ID!): Post
+        getPost(postId: ID!): Post
     }
 
     type Mutation {
@@ -33,5 +50,12 @@ ad        getPost(postId: ID!): Post
         login(username: String!, password: String!): User!
         createPost(body: String!): Post!
         deletePost(postId: ID!): String!
+        createComment(postId: ID!, body: String!): Post!
+        deleteComment(postId: ID!, commentId: ID!): Post!
+        likePost(postId: ID!): Post!
+    }
+
+    type Subscription {
+        newPost: Post!
     }
 `;
