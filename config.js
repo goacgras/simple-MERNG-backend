008cc04ecc30c84b253ed3fb2201d1812f536784
Reza Ghifary <fachrezaghifary@gmail.com>
2020-12-16 16:26:30 +0700

    ready to deploy

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: `mongodb+srv://gras:JD28eqgi2iF1EEdA@gras-clusters.dh5gg.gcp.mongodb.net/simple-MERNG?retryWrites=true&w=majority`,
+    SECRET_KEY: 'goacgras is the best'
+};
